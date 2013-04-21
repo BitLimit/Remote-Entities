5a704b9d3897ddcf08972db8bdf1a78c05eae3fa
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-04-21 15:12:28 -0700

    Fix NPEs in YMLSerializer.

--- src/main/java/de/kumpelblase2/remoteentities/persistence/serializers/YMLSerializer.java
+++ src/main/java/de/kumpelblase2/remoteentities/persistence/serializers/YMLSerializer.java
@@ -1,6 +1,9 @@
 package de.kumpelblase2.remoteentities.persistence.serializers;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -12,115 +15,126 @@ import de.kumpelblase2.remoteentities.persistence.ISingleEntitySerializer;
 
 public class YMLSerializer extends PreparationSerializer implements ISingleEntitySerializer
 {
-	protected FileConfiguration m_config;
-	protected File m_configFile;
-	
-	public YMLSerializer(Plugin inPlugin)
-	{
-		super(inPlugin);
-		ConfigurationSerialization.registerClass(EntityData.class);
-	}
+    protected FileConfiguration m_config;
+    protected File m_configFile;
 
-	@Override
-	public boolean save(EntityData[] inData)
-	{
-		try
-		{
-			if(this.m_config == null || this.m_configFile == null)
-			{
-				if(!this.loadConfig())
-					return false;
-			}
-			
-			this.m_config.set("entities", inData);
-			this.m_config.save(this.m_configFile);
-			
-			return true;
-		}
-		catch(Exception e)
-		{
-			return false;
-		}
-	}
+    public YMLSerializer(Plugin inPlugin)
+    {
+        super(inPlugin);
+        ConfigurationSerialization.registerClass(EntityData.class);
+    }
 
-	@Override
-	public EntityData[] loadData()
-	{
-		if(this.m_configFile == null || this.m_config == null)
-		{
-			if(!this.loadConfig())
-				return new EntityData[0];
-		}
-		
-		return this.m_config.getList("entities").toArray(new EntityData[0]);
-	}
+    @Override
+    public boolean save(EntityData[] inData)
+    {
+        try
+        {
+            if(this.m_config == null || this.m_configFile == null)
+            {
+                if(!this.loadConfig())
+                    return false;
+            }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void save(EntityData inData)
-	{
-		try
-		{
-			if(this.m_config == null || this.m_configFile == null)
-			{
-				if(!this.loadConfig())
-					return;
-			}
-			
-			((List<EntityData>)this.m_config.getList("entities")).add(inData);
-			this.m_config.save(this.m_configFile);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            this.m_config.set("entities", inData);
+            this.m_config.save(this.m_configFile);
 
-	@Override
-	public EntityData load(Object inParameter)
-	{
-		EntityData[] data = this.loadData();
-		for(EntityData entity : data)
-		{
-			if(inParameter instanceof String)
-			{
-				if(inParameter.equals(entity.name))
-					return entity;
-			}
-			else if(inParameter instanceof Integer)
-			{
-				if(inParameter.equals((Integer)entity.id))
-					return entity;
-			}
-		}
-		return null;
-	}
-	
-	protected boolean loadConfig()
-	{
-		try
-		{
-			File fileFolder = new File(RemoteEntities.getInstance().getDataFolder(), this.m_plugin.getName());
-			if(!fileFolder.exists())
-			{
-				if(!fileFolder.mkdirs())
-					return false;
-			}
-			
-			this.m_configFile = new File(fileFolder, "entities.yml");
-			if(!this.m_configFile.exists())
-			{
-				if(!this.m_configFile.createNewFile())
-					return false;
-			}
-		
-			this.m_config = YamlConfiguration.loadConfiguration(this.m_configFile);
-			return true;
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			return false;
-		}
-	}
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public EntityData[] loadData()
+    {
+        if(this.m_configFile == null || this.m_config == null)
+        {
+            if(!this.loadConfig())
+                return new EntityData[0];
+        } else if (!this.m_config.isList("entities")) {
+            this.m_config.set("entities", new EntityData[0]);
+            try {
+                this.m_config.save(this.m_configFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return this.m_config.getList("entities").toArray(new EntityData[0]);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void save(EntityData inData)
+    {
+        try
+        {
+            if(this.m_config == null || this.m_configFile == null)
+            {
+                if(!this.loadConfig())
+                    return;
+            }
+
+            if (this.m_config.isList("entities"))
+                ((List<EntityData>)this.m_config.getList("entities")).add(inData);
+            else
+                this.m_config.set("entities", new ArrayList<EntityData>(Arrays.asList(inData)));
+
+            this.m_config.save(this.m_configFile);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public EntityData load(Object inParameter)
+    {
+        EntityData[] data = this.loadData();
+        for(EntityData entity : data)
+        {
+            if(inParameter instanceof String)
+            {
+                if(inParameter.equals(entity.name))
+                    return entity;
+            }
+            else if(inParameter instanceof Integer)
+            {
+                if(inParameter.equals((Integer)entity.id))
+                    return entity;
+            }
+        }
+        return null;
+    }
+
+    protected boolean loadConfig()
+    {
+        try
+        {
+            File fileFolder = new File(RemoteEntities.getInstance().getDataFolder(), this.m_plugin.getName());
+            if(!fileFolder.exists())
+            {
+                if(!fileFolder.mkdirs())
+                    return false;
+            }
+
+            this.m_configFile = new File(fileFolder, "entities.yml");
+            if(!this.m_configFile.exists())
+            {
+                if(!this.m_configFile.createNewFile())
+                    return false;
+            }
+
+            this.m_config = YamlConfiguration.loadConfiguration(this.m_configFile);
+            return true;
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
